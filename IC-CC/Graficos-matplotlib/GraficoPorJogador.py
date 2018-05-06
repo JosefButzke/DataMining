d93c32da8e182c01e9b39d3052d49658bc070005
Josef Stein Butzke <josef-stein@hotmail.com>
2018-05-06 20:37:56 -0300

    atualizado exibição tempo

--- IC-CC/Graficos-matplotlib/GraficoPorJogador.py
+++ IC-CC/Graficos-matplotlib/GraficoPorJogador.py
@@ -13,9 +13,12 @@ alunosLista = sorted([list(x) for x in set(tuple(x) for x in df[['Codigo_jogador
 alunoSelecionado = int(input("Escolha o aluno:" + str(alunosLista) + "\n=>"))
 
 aluno = (df['Codigo_jogador']).tolist()
+
 x = []
 y = []
+
 cont = 0
+
 for i in range(len(aluno)):
     if aluno[i] is alunoSelecionado:
         x.append(df['Codigo_expressao'][i])
@@ -27,20 +30,30 @@ w = []
 for i in range(len(x)):
     w.append([x[i],y[i]])
 
-
 w.sort(key=lambda x: x[1])
 
+import time
+
+t = []
+r = 0
+print(w)
+for i in range(len(w)):
+    m,s = divmod(w[i][1], 60)
+    t.append([w[i][0],m])
+
+print(t)
 x = []
 y = []
 
-for i in range(len(w)):
-    x.append(w[i][0])
-    y.append(w[i][1])
+for i in range(len(t)):
+    x.append(t[i][0])
+    y.append(t[i][1])
 
 plt.plot(y,x, color='red')
+plt.axis([0, 200, 1 , 6])
 plt.xlabel('Hora_captura')
 plt.ylabel('Codigo_expressao')
 plt.title('Hora_captura X Codigo_expressao')
-plt.grid(True, color='g', linewidth="3")
+plt.grid(True, color='g', linewidth="1")
 plt.show()
 
